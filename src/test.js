0ea90823e41fce078c3b6ecc47fe1940e46842d4
MARS <Sergei-6@bk.ru>
2022-10-16 22:48:33 +0300

    feat: common character count - done

--- src/test.js
+++ src/test.js
@@ -60,6 +60,7 @@ function encodeLine(str) {
 console.log(encodeLine('abbcca'));
 */
 
+/*
 function transform(arr) {
     let newArray = [];
 
@@ -127,6 +128,27 @@ function transform(arr) {
 }
 
 
-
 transform(['--double-prev', 1, 2, 3]);
 transform([1, 2, 3, '--double-next']);
+*/
+
+/*
+function getCommonCharacterCount(s1, s2) {
+
+    let count = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s2.includes(s1[i])) {
+            console.log(s1[i]);
+            count++;
+            s2 = s2.replace(s1[i], '');
+        }
+    }
+    console.log(s1);
+    console.log(s2);
+    return count;
+
+}
+
+console.log(getCommonCharacterCount('aabcc', 'adcaa'));
+*/
